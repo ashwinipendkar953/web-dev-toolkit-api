d008a973c7b29eaf5dbf981da5869bd299cb2532
Ashwini Pendkar <ashwinipendkar953@gmail.com>
2024-07-16 17:01:01 +0530

    feat: added timestamp

--- models/projects.models.js
+++ models/projects.models.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
 
-const projectSchema = new mongoose.Schema({
-  projectName: String,
-  projectLiveUrl: String,
-  category: String,
-  description: String,
-  replUrl: String,
-  githubUrl: String,
-});
+const projectSchema = new mongoose.Schema(
+  {
+    projectName: String,
+    projectLiveUrl: String,
+    category: String,
+    description: String,
+    replUrl: String,
+    githubUrl: String,
+  },
+  { timestamps: true }
+);
 
 const Project = mongoose.model("Project", projectSchema);
 
